0c4e20c3ed3352da454c98dead403c0426f3c0cc
Prokhor Portugalsky <prohor.luv@gmail.com>
2022-11-27 14:48:32 +0000

    4.2 Маршрутизатор

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/ktm_125', function(req, res, next) {
+  res.send("<h1>KTM 125</h1>")
+});
+
+router.get('/ktm_250', function(req, res, next) {
+  res.send("<h1>KTM 250</h1>")
+});
+
+router.get('/ktm_300', function(req, res, next) {
+  res.send("<h1>KTM 300</h1>")
+});
+
 module.exports = router;
