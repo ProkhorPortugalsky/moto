31ec226dbb8b73bb9c80af2d67d3273eecded9c7
Prokhor Portugalsky <prohor.luv@gmail.com>
2022-11-27 15:09:22 +0000

    4.3 Создание Шаблона

--- routes/index.js
+++ routes/index.js
@@ -7,15 +7,15 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/ktm_125', function(req, res, next) {
-  res.send("<h1>KTM 125</h1>")
+  res.render('moto', { title: 'KTM 125' , picture: 'images/125.jpg', desc: 'Характерный мотоцикл KTM 125 EXC построен специально для поклонников эндуро, предпочитающих ломать стереотипы при движении в городе, всегда готовых отправиться в настоящее путешествие' });
 });
 
 router.get('/ktm_250', function(req, res, next) {
-  res.send("<h1>KTM 250</h1>")
+  res.render('moto', { title: 'KTM 250' , picture: 'images/250.jpg', desc: 'KTM 250 EXC TPI нового поколения представляет собой идеальное сочетание мощности и управляемости и устанавливает четкий стандарт интуитивного управления, уникального стиля и максимальной производительности' });
 });
 
 router.get('/ktm_300', function(req, res, next) {
-  res.send("<h1>KTM 300</h1>")
+  res.render('moto', { title: 'KTM 300' , picture: 'images/300.jpg', desc: 'KTM 300 EXC был в авангарде безумных темпов развития экстремального enduro самого высокого уровня. Этот маневренный и легкий мастер-вездеход не боится препятствий и с появлением технологии TPI поднялся на невероятные высоты' });
 });
 
 module.exports = router;
