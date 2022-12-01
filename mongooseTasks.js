9cff2869bc1dcb9d08c82751dd5eb24ca057ab1b
Prokhor Portugalsky <prohor.luv@gmail.com>
2022-12-01 14:59:51 +0000

    7.2 Создание схемы

--- mongooseTasks.js
+++ mongooseTasks.js
@@ -1,13 +1,20 @@
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://0.0.0.0/test')
 
-var Mot = mongoose.model('Honda', { name: String })
+var schema = mongoose.Schema({ name: String })
+
+schema.methods.frrr = function(){
+    console.log(this.get("name")+ "sounds loud!")
+} 
+
+var Mot = mongoose.model('Honda', schema)
 
 var Motty = new Mot({ name: 'CRF' })
 Motty.save(function (err) {
     if (err) {
         console.log(err)
     } else {
-        console.log('FRRRRRR')
+        Motty.frrr();
     }
 })
+
