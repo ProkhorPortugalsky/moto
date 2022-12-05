d7ecee90db7a1e2d7965c668e4ad039ebb10eee1
Prokhor Portugalsky <prohor.luv@gmail.com>
2022-12-05 13:49:06 +0000

    8.2 Обработка параметра в адресе

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var motosRouter = require('./routes/motos');
 
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://0.0.0.0/motos');
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/motos', motosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
