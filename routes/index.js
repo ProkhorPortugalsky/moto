baee1f110b70f81855e6e3c79ec0df61ab85e24c
Prokhor Portugalsky <prohor.luv@gmail.com>
2022-12-25 14:05:26 +0000

    8.5 Заполнение меню из базы

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var Mot = require("../models/mot").Mot
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  Mot.find({},{_id:0,title:1,nick:1}, function(err, menu){
+    res.render('index', { title: 'Express', menu: menu });
+})
 });
 module.exports = router;
--- routes/motos.js
+++ routes/motos.js
@@ -1,18 +1,37 @@
 var express = require('express');
 var router = express.Router();
 var Mot = require("../models/mot").Mot
+var async = require("async");
 
-/* Страница моделей */
+// /* Страница моделей */
 router.get('/:nick', function(req, res, next) {
-    Mot.findOne({nick:req.params.nick}, function(err,mot){
+    async.parallel([
+        function(callback){
+            Mot.findOne({nick:req.params.nick},function(err,mot){
+                callback(err,mot)
+            })
+    
+        },
+        function(callback){
+            Mot.find({},{_id:0,title:1,nick:1}, function(err,mots){
+                callback(err,mots)
+            })
+        }
+    ],
+    function(err,result){
         if(err) return next(err)
-        if(!mot) return next(new Error("Нет такой модели"))
+        var mot = result[0];
+        var mots = result[1]
+        if(!mot) return next(new Error("Нет такой модели КТМ"))
+        //console.log(mot.avatar)
         res.render('moto', {
             title: mot.title,
             picture: mot.avatar,
-            desc: mot.desc
-        })
+            desc: mot.desc,
+            menu: mots
+        });
     })
 })
+
 
 module.exports = router;
