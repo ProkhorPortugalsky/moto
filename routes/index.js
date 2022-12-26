1cc0428422e6b447e479112fbdeced7de666b277
Prokhor Portugalsky <prohor.luv@gmail.com>
2022-12-26 15:06:51 +0000

    9.3 Сохранение Сесси в БД

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,8 @@ var Mot = require("../models/mot").Mot
 /* GET home page. */
 router.get('/', function(req, res, next) {
   Mot.find({},{_id:0,title:1,nick:1}, function(err, menu){
-    res.cookie('greeting', 'Hi!!!').render('index', { title: 'Express', menu: menu });
+    req.session.greeting="Hi!!!";
+    res.render('index', { title: 'Express', menu: menu });
 })
 });
 module.exports = router;
