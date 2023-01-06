38bcae45dcd95de7ac8d9495b0ba55f8a927bb0a
Prokhor Portugalsky <prohor.luv@gmail.com>
2023-01-06 14:37:22 +0000

    10.6 Ошибка аутентификации

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ router.get('/', function(req, res, next) {
 
 /* GET login/registration page. */
 router.get('/logreg', function(req, res, next) {
-  res.render('logreg',{title: 'Вход'});
+  res.render('logreg',{title: 'Вход', error:null});
   });  
 
   /* POST login/registration page. */
@@ -26,7 +26,7 @@ router.post('/logreg', function(req, res, next) {
         req.session.user = user._id
         res.redirect('/')
       } else {
-        res.render('logreg', {title: 'Вход'})
+        res.render('logreg', {title: 'Ошибка', error:"Пароль не верный"})
       }       
     } else {
       var user = new User({username:username,password:password})
